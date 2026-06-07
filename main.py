186b1ffd3477795faf11719fa26c4e7e464bc71f
Mirelle da Silva <mirellealessandredasilva@gmail.com>
2026-06-07 06:44:29 -0300

    Refactor: main deve iniciar a aplicação

---
+++ main.py
@@ -0,0 +1,8 @@
+from atendimento_controller import AtendimentoController
+
+def main():
+    controller = AtendimentoController()
+    controller.iniciar_atendimento()
+
+if __name__ == "__main__":
+    main()
