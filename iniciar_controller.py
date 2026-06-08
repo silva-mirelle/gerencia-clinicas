d3dcb1c877e03c4e1c09f70ca91d2ea611ddc279
Mirelle da Silva <mirellealessandredasilva@gmail.com>
2026-06-07 21:57:50 -0300

    Reorganiza o menu em submenus por area (menu principal enxuto)
    - Menu principal: 8 areas (Atendimento, Clinicas, Pacientes, Profissionais,
      Tipos, Procedimentos, Pagamentos, Relatorios) + Encerrar
    - Cada area abre um submenu com suas acoes (CRUD); '0 - Voltar' retorna ao principal
    - ViewIniciar.submenu(titulo, opcoes) generico, reusado por todas as areas
    - Remove o metodo morto registrar_atendimento
    - Comportamento das operacoes inalterado (so reorganizacao da navegacao)

--- iniciar_controller.py
+++ iniciar_controller.py
@@ -2,6 +2,7 @@
 # E o "maestro" do sistema: dono do menu raiz, cria UM SistemaClinicas e o
 # injeta em todos os controllers especificos (injecao de dependencia) para que
 # todos compartilhem o mesmo "banco de dados" em memoria.
+# O menu e em 2 niveis: menu principal (por area) -> submenu (acoes da area).
 from view_iniciar import ViewIniciar
 from registro_atendimento_controller import RegistroAtendimentoController
 from sistema_clinicas import SistemaClinicas
@@ -27,7 +28,6 @@ class IniciarController:
         self.__registro_procedimento_controller = RegistroProcedimentoController(self.__sistema_clinicas)
         self.__relatorio_controller = RelatorioController(self.__sistema_clinicas)
         self.__registro_pagamento_controller = RegistroPagamentoController(self.__sistema_clinicas)
-        # o controller de registro recebe o sistema + os controllers ja prontos
         self.__registro_atendimento_controller = RegistroAtendimentoController(
             self.__sistema_clinicas,
             self.__cadastrar_clinica_controller,
@@ -35,82 +35,183 @@ class IniciarController:
         )
 
     def iniciar_atendimento(self):
-        # laco do menu principal: pergunta a opcao a tela e roteia para a acao.
-        # Cada opcao delega para um controller especifico (o maestro nao faz a regra).
+        # MENU PRINCIPAL: cada opcao abre o submenu de uma area.
         while True:
             escolha = self.__view_iniciar.menu()
             if escolha == '1':
-                idade = self.__ler_idade()
-                if self.valida_idade(idade):
-                    self.__registro_atendimento_controller.iniciar_registro()
-                else:
-                    # menor de idade: barra e reinicia o menu
-                    self.__view_iniciar.invalida_atendimento()
+                self.__menu_atendimento()
+            elif escolha == '2':
+                self.__menu_clinicas()
+            elif escolha == '3':
+                self.__menu_pacientes()
             elif escolha == '4':
-                self.__cadastrar_profissional_controller.cadastrar()
+                self.__menu_profissionais()
             elif escolha == '5':
-                self.__cadastrar_clinica_controller.listar()
+                self.__menu_tipos()
             elif escolha == '6':
-                self.__cadastrar_paciente_controller.listar()
+                self.__menu_procedimentos()
             elif escolha == '7':
-                self.__cadastrar_clinica_controller.alterar()
+                self.__menu_pagamentos()
             elif escolha == '8':
-                self.__cadastrar_clinica_controller.excluir()
-            elif escolha == '9':
-                self.__cadastrar_paciente_controller.alterar()
-            elif escolha == '10':
-                self.__cadastrar_paciente_controller.excluir()
-            elif escolha == '11':
-                self.__cadastrar_tipo_atendimento_controller.cadastrar()
-            elif escolha == '12':
-                self.__cadastrar_tipo_atendimento_controller.listar()
-            elif escolha == '13':
-                self.__cadastrar_tipo_atendimento_controller.alterar()
-            elif escolha == '14':
-                self.__cadastrar_tipo_atendimento_controller.excluir()
-            elif escolha == '15':
-                self.__cadastrar_profissional_controller.listar()
-            elif escolha == '16':
-                self.__cadastrar_profissional_controller.alterar()
-            elif escolha == '17':
-                self.__cadastrar_profissional_controller.excluir()
-            elif escolha == '18':
-                self.__registro_procedimento_controller.registrar()
-            elif escolha == '19':
-                self.__registro_procedimento_controller.listar()
-            elif escolha == '20':
-                self.__relatorio_controller.procedimentos_mais_realizados()
-            elif escolha == '21':
-                self.__relatorio_controller.procedimentos_caros_baratos()
-            elif escolha == '22':
-                self.__registro_pagamento_controller.registrar()
-            elif escolha == '23':
-                self.__registro_pagamento_controller.listar()
-            elif escolha == '24':
-                self.__registro_atendimento_controller.listar()
-            elif escolha == '25':
-                self.__registro_atendimento_controller.alterar()
-            elif escolha == '26':
-                self.__registro_atendimento_controller.excluir()
-            elif escolha == '27':
-                self.__registro_procedimento_controller.alterar()
-            elif escolha == '28':
-                self.__registro_procedimento_controller.excluir()
-            elif escolha == '29':
-                self.__registro_pagamento_controller.alterar()
-            elif escolha == '30':
-                self.__registro_pagamento_controller.excluir()
-            elif escolha == '31':
-                self.__relatorio_controller.clinicas_mais_atendimentos()
-            elif escolha == '32':
-                self.__relatorio_controller.atendimentos_caros_baratos()
+                self.__menu_relatorios()
             elif escolha == '0':
-                break  # sai do laco -> metodo retorna -> programa encerra
+                break  # sai do laco -> programa encerra
             else:
                 self.__view_iniciar.opcao_invalida()
 
+    # ---- SUBMENUS (cada um roteia para o controller da area) ----
+
+    def __menu_atendimento(self):
+        while True:
+            escolha = self.__view_iniciar.submenu(
+                "ATENDIMENTO", ["Registrar atendimento", "Listar", "Alterar", "Excluir"])
+            if escolha == '1':
+                self.__registrar_atendimento()
+            elif escolha == '2':
+                self.__registro_atendimento_controller.listar()
+            elif escolha == '3':
+                self.__registro_atendimento_controller.alterar()
+            elif escolha == '4':
+                self.__registro_atendimento_controller.excluir()
+            elif escolha == '0':
+                break
+            else:
+                self.__view_iniciar.opcao_invalida()
+
+    def __menu_clinicas(self):
+        # cadastro de clinica acontece no fluxo de atendimento (quando nao existe);
+        # aqui ficam listar/alterar/excluir.
+        while True:
+            escolha = self.__view_iniciar.submenu(
+                "CLINICAS", ["Listar", "Alterar", "Excluir"])
+            if escolha == '1':
+                self.__cadastrar_clinica_controller.listar()
+            elif escolha == '2':
+                self.__cadastrar_clinica_controller.alterar()
+            elif escolha == '3':
+                self.__cadastrar_clinica_controller.excluir()
+            elif escolha == '0':
+                break
+            else:
+                self.__view_iniciar.opcao_invalida()
+
+    def __menu_pacientes(self):
+        # cadastro de paciente acontece no fluxo de atendimento; aqui o resto do CRUD.
+        while True:
+            escolha = self.__view_iniciar.submenu(
+                "PACIENTES", ["Listar", "Alterar", "Excluir"])
+            if escolha == '1':
+                self.__cadastrar_paciente_controller.listar()
+            elif escolha == '2':
+                self.__cadastrar_paciente_controller.alterar()
+            elif escolha == '3':
+                self.__cadastrar_paciente_controller.excluir()
+            elif escolha == '0':
+                break
+            else:
+                self.__view_iniciar.opcao_invalida()
+
+    def __menu_profissionais(self):
+        while True:
+            escolha = self.__view_iniciar.submenu(
+                "PROFISSIONAIS", ["Cadastrar", "Listar", "Alterar", "Excluir"])
+            if escolha == '1':
+                self.__cadastrar_profissional_controller.cadastrar()
+            elif escolha == '2':
+                self.__cadastrar_profissional_controller.listar()
+            elif escolha == '3':
+                self.__cadastrar_profissional_controller.alterar()
+            elif escolha == '4':
+                self.__cadastrar_profissional_controller.excluir()
+            elif escolha == '0':
+                break
+            else:
+                self.__view_iniciar.opcao_invalida()
+
+    def __menu_tipos(self):
+        while True:
+            escolha = self.__view_iniciar.submenu(
+                "TIPOS DE ATENDIMENTO", ["Cadastrar", "Listar", "Alterar", "Excluir"])
+            if escolha == '1':
+                self.__cadastrar_tipo_atendimento_controller.cadastrar()
+            elif escolha == '2':
+                self.__cadastrar_tipo_atendimento_controller.listar()
+            elif escolha == '3':
+                self.__cadastrar_tipo_atendimento_controller.alterar()
+            elif escolha == '4':
+                self.__cadastrar_tipo_atendimento_controller.excluir()
+            elif escolha == '0':
+                break
+            else:
+                self.__view_iniciar.opcao_invalida()
+
+    def __menu_procedimentos(self):
+        while True:
+            escolha = self.__view_iniciar.submenu(
+                "PROCEDIMENTOS", ["Registrar", "Listar", "Alterar", "Excluir"])
+            if escolha == '1':
+                self.__registro_procedimento_controller.registrar()
+            elif escolha == '2':
+                self.__registro_procedimento_controller.listar()
+            elif escolha == '3':
+                self.__registro_procedimento_controller.alterar()
+            elif escolha == '4':
+                self.__registro_procedimento_controller.excluir()
+            elif escolha == '0':
+                break
+            else:
+                self.__view_iniciar.opcao_invalida()
+
+    def __menu_pagamentos(self):
+        while True:
+            escolha = self.__view_iniciar.submenu(
+                "PAGAMENTOS", ["Registrar", "Listar", "Alterar", "Excluir"])
+            if escolha == '1':
+                self.__registro_pagamento_controller.registrar()
+            elif escolha == '2':
+                self.__registro_pagamento_controller.listar()
+            elif escolha == '3':
+                self.__registro_pagamento_controller.alterar()
+            elif escolha == '4':
+                self.__registro_pagamento_controller.excluir()
+            elif escolha == '0':
+                break
+            else:
+                self.__view_iniciar.opcao_invalida()
+
+    def __menu_relatorios(self):
+        while True:
+            escolha = self.__view_iniciar.submenu("RELATORIOS", [
+                "Procedimentos mais realizados",
+                "Procedimentos mais caros e mais baratos",
+                "Clinicas com mais atendimentos",
+                "Atendimentos mais caros e mais baratos",
+            ])
+            if escolha == '1':
+                self.__relatorio_controller.procedimentos_mais_realizados()
+            elif escolha == '2':
+                self.__relatorio_controller.procedimentos_caros_baratos()
+            elif escolha == '3':
+                self.__relatorio_controller.clinicas_mais_atendimentos()
+            elif escolha == '4':
+                self.__relatorio_controller.atendimentos_caros_baratos()
+            elif escolha == '0':
+                break
+            else:
+                self.__view_iniciar.opcao_invalida()
+
+    # ---- helpers do atendimento ----
+
+    def __registrar_atendimento(self):
+        # valida a idade ANTES de seguir; so maior de 18 registra de forma independente
+        idade = self.__ler_idade()
+        if self.valida_idade(idade):
+            self.__registro_atendimento_controller.iniciar_registro()
+        else:
+            self.__view_iniciar.invalida_atendimento()
+
     def __ler_idade(self):
-        # le a idade da tela e repete ate ser um inteiro valido.
+        # le a idade e repete ate ser um inteiro valido.
         # try/except trata a entrada invalida (ex.: "abc" faria int() lancar ValueError).
         while True:
             idade = self.__view_iniciar.idade_user()
@@ -122,9 +223,3 @@ class IniciarController:
     def valida_idade(self, idade: int):
         # regra de negocio: so maior de 18 pode atendimento independente
         return idade > 18
-
-    def registrar_atendimento(self):
-        # TODO: codigo morto/placeholder - Atendimento exige 8 argumentos e nao
-        # esta importado aqui. Mantido para implementacao futura; nao e chamado.
-        atendimento = Atendimento()
-        return atendimento
--- view_iniciar.py
+++ view_iniciar.py
@@ -1,52 +1,38 @@
 # VIEW (Tela) do menu principal (MVC).
-# So faz entrada/saida com o usuario (print/input). Nenhuma regra de negocio:
-# quem decide o que cada opcao faz e o IniciarController.
+# So faz entrada/saida. O menu agora e em 2 niveis: um menu principal por AREA
+# e submenus com as acoes de cada area (deixa o menu enxuto).
 class ViewIniciar:
+
     def menu(self):
-        # mostra as opcoes e devolve a escolha (string) ao controller
-        print("TELA DE ATENDIMENTO")
-        print("1 - Registrar atendimento")
-        print("2 - Cadastrar paciente")
-        print("3 - Cadastrar clínica")
-        print("4 - Cadastrar profissional de saúde")
-        print("5 - Listar clinicas")
-        print("6 - Listar pacientes")
-        print("7 - Alterar clinica")
-        print("8 - Excluir clinica")
-        print("9 - Alterar paciente")
-        print("10 - Excluir paciente")
-        print("11 - Cadastrar tipo de atendimento")
-        print("12 - Listar tipos de atendimento")
-        print("13 - Alterar tipo de atendimento")
-        print("14 - Excluir tipo de atendimento")
-        print("15 - Listar profissionais")
-        print("16 - Alterar profissional")
-        print("17 - Excluir profissional")
-        print("18 - Registrar procedimento")
-        print("19 - Listar procedimentos")
-        print("20 - Relatorio: procedimentos mais realizados")
-        print("21 - Relatorio: procedimentos mais caros e mais baratos")
-        print("22 - Registrar pagamento")
-        print("23 - Listar pagamentos")
-        print("24 - Listar atendimentos")
-        print("25 - Alterar atendimento")
-        print("26 - Excluir atendimento")
-        print("27 - Alterar procedimento")
-        print("28 - Excluir procedimento")
-        print("29 - Alterar pagamento")
-        print("30 - Excluir pagamento")
-        print("31 - Relatorio: clinicas com mais atendimentos")
-        print("32 - Relatorio: atendimentos mais caros e mais baratos")
+        # menu principal: so as AREAS (o detalhe fica nos submenus)
+        print("\n=== SISTEMA DE CLINICAS ===")
+        print("1 - Atendimento")
+        print("2 - Clinicas")
+        print("3 - Pacientes")
+        print("4 - Profissionais")
+        print("5 - Tipos de atendimento")
+        print("6 - Procedimentos")
+        print("7 - Pagamentos")
+        print("8 - Relatorios")
         print("0 - Encerrar")
-        return input("Escolha um numero: ")
+        return input("Escolha: ")
+
+    def submenu(self, titulo, opcoes):
+        # submenu generico: recebe o titulo e a lista de acoes, numera e le a escolha.
+        # "0 - Voltar" volta pro menu principal.
+        print(f"\n--- {titulo} ---")
+        for i, opcao in enumerate(opcoes):
+            print(f"{i + 1} - {opcao}")
+        print("0 - Voltar")
+        return input("Escolha: ")
 
     def idade_user(self):
         # pede a idade (validacao numerica e feita no controller)
-        print("VALIDAÇÃO IDADE PARA REGISTRAR ATENDIMENTO")
+        print("VALIDACAO DE IDADE PARA REGISTRAR ATENDIMENTO")
         return input("Idade: ")
 
     def invalida_atendimento(self):
-        print("MENORES PRECISAM QUE UM RESPONSÁVEL REGISTRE O ATENDIMENTO")
+        print("MENORES PRECISAM QUE UM RESPONSAVEL REGISTRE O ATENDIMENTO")
 
     def erro_idade_invalida(self):
         print("Idade invalida: digite apenas numeros inteiros.")
